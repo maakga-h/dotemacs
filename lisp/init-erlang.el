27610483514762edbc9453255563afac5b63b0f1
maakga-h <64649667+maakga-h@users.noreply.github.com>
2021-01-03 22:16:42 +0800

    move erlang var to custom

--- lisp/init-erlang.el
+++ lisp/init-erlang.el
@@ -1,7 +1,6 @@
 
-(setq load-path (cons  "/root/.asdf/installs/erlang/22.3/lib/tools-3.3.1/emacs" load-path))
-(setq erlang-root-dir "/root/.asdf/installs/erlang/22.3")
-(setq exec-path (cons "/root/.asdf/installs/erlang/22.3/bin" exec-path))
-(require 'erlang-start)
+(if (boundp 'erlang-root-dir)
+    (require 'erlang-start)
+  )
 
 (provide 'init-erlang)
