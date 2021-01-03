5897bf8c11b3a221db656d2ab6c29f8fc4e2a795
maakga-h <64649667+maakga-h@users.noreply.github.com>
2021-01-03 22:03:20 +0800

    add tldr

--- lisp/init-utils.el
+++ lisp/init-utils.el
@@ -2,8 +2,11 @@
   :bind (("C-c y" . youdao-dictionary-search-at-point))
   :config
   (setq url-automatic-caching t)
-  ;;(push "*Youdao Dictionary*" popwin:special-display-config)
-  
+  ;;(push "*Youdao Dictionary*" popwin:special-display-config)  
   )
+
+
+(use-package tldr)
+  
 
 (provide 'init-utils)
