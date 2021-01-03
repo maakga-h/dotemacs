458e92e5daf9356c145d6c044e9da5603fed8726
maakga-h <64649667+maakga-h@users.noreply.github.com>
2021-01-03 22:29:07 +0800

    fix error when not define my/lsp

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -1,6 +1,8 @@
-(pcase my/lsp
-  ('lsp-mode (require 'init-lsp-lsp-mode))
-  ('eglot (require 'init-lsp-eglot))
-    )
+(if (boundp my/lsp)
+    (pcase my/lsp
+      ('lsp-mode (require 'init-lsp-lsp-mode))
+      ('eglot (require 'init-lsp-eglot))
+      )    
+  )
 
 (provide 'init-lsp)
