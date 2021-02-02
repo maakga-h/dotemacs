aad9c8c6f911d0edd12d23b8f4b2e0fa48c1e99b
maakga-h <64649667+maakga-h@users.noreply.github.com>
2021-02-03 00:00:45 +0800

    nov + visual-fill-column

--- lisp/init-utils.el
+++ lisp/init-utils.el
@@ -24,8 +24,19 @@
 
 (use-package undo-tree)
 
+(use-package visual-fill-column
+  ;;:hook
+  ;;(visual-fill-column-mode. visual-line-mode)
+  )
+
 (use-package nov
-  :hook (nov-mode . (lambda () (display-line-numbers-mode -1)))
+  :hook
+  (nov-mode . (lambda () (display-line-numbers-mode -1)))
+  (nov-mode . visual-line-mode)
+  (nov-mode . visual-fill-column-mode)  
+  :config
+  (setq nov-text-width t)
+;;  (setq visual-fill-column-center-text t)
   )
 (add-to-list 'auto-mode-alist '("\\.epub\\'" . nov-mode))
 
