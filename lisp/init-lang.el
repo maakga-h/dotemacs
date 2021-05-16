9732066d452612457d07cd5e8af53f311545ac7d
maakga-h <64649667+maakga-h@users.noreply.github.com>
2021-05-17 02:17:56 +0800

    read file auto decode, save file use utf-8

---
+++ lisp/init-lang.el
@@ -0,0 +1,23 @@
+
+;; http://ergoemacs.org/emacs/emacs_encoding_decoding_faq.html
+(set-language-environment "UTF-8")
+;; ;;;windows system
+;; ;;fuck notify you for settning https://github.com/sriramkswamy/ryo-emacs/blob/master/init.el
+;; (setq coding-system-for-read 'utf-8)										  	; use utf-8 by default for reading
+ (setq coding-system-for-write 'utf-8) ;; 保存文件用utf8
+;; (set-language-environment 'utf-8)
+;; (set-terminal-coding-system 'utf-8)
+;; (setq-default pathname-coding-system 'utf-8)
+;; (setq default-process-coding-system 
+;;             '(utf-8 . utf-8))
+;; (set-buffer-file-coding-system 'utf-8)
+;; (set-keyboard-coding-system 'utf-8)
+;; ;(set-clipboard-coding-system 'utf-8)
+
+;; 读文件自动转码
+(use-package unicad
+  :init (unicad-mode)
+  )
+
+(provide 'init-lang)
+;;; init-lang.el ends here
