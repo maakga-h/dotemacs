c81f071af31313c90eeddf59c25b37dbf286160b
maakga-h <64649667+maakga-h@users.noreply.github.com>
2022-04-15 14:51:03 +0800

    add general & def key

--- lisp/init-vim.el
+++ lisp/init-vim.el
@@ -3,10 +3,36 @@
 (use-package evil
   :defer 1 
   :hook (after-init . evil-mode)
-;;  :config
-  ;;(evil-mode t)
+  :config
+  ;;(evil-set-leader 'normal (kbd "SPC"))
+  ;;(evil-define-key 'normal 'global (kbd "<leader>r") 'counsel-rg)
+  ;;(evil-define-key 'normal 'global (kbd "<leader>q") 'counsel-ag-thing-at-point)
   ;;:custom
   ;;(evil-search-module 'isearch)
   )
+
+(use-package general)
+
+(general-define-key
+ :states '(normal)
+ :keymaps 'override
+ :prefix "SPC"
+
+ "SPC" 'execute-extended-command
+ "q" 'counsel-ag-thing-at-point
+ "r" 'counsel-rg
+
+ "ff" 'counsel-find-file
+ "fF" 'find-file-other-window
+ "fr" 'counsel-recentf
+
+ "gf" 'counsel-git
+
+ "bb" 'ivy-switch-buffer
+ "b/" 'switch-to-buffer-other-window
+ "bk" 'kill-buffer
+
+ "/" 'split-window-horizontally
+ )
 
 (provide 'init-vim)
