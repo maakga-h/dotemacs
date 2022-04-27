99c51eba1cdeac871b15763d71227f4f96ccbd84
maakga-h <64649667+maakga-h@users.noreply.github.com>
2022-04-27 20:38:28 +0800

    add which key prefix

--- lisp/init-vim.el
+++ lisp/init-vim.el
@@ -9,6 +9,24 @@
   ;;(evil-define-key 'normal 'global (kbd "<leader>q") 'counsel-ag-thing-at-point)
   ;;:custom
   ;;(evil-search-module 'isearch)
+ ;; esc quits
+  (defun minibuffer-keyboard-quit ()
+	"Abort recursive edit.
+In Delete Selection mode, if the mark is active, just deactivate it;
+then it takes a second \\[keyboard-quit] to abort the minibuffer."
+	(interactive)
+	(if (and delete-selection-mode transient-mark-mode mark-active)
+		(setq deactivate-mark  t)
+      (when (get-buffer "*Completions*") (delete-windows-on "*Completions*"))
+      (abort-recursive-edit)))
+  ;; (define-key evil-normal-state-map [escape] 'keyboard-quit)
+  ;; (define-key evil-visual-state-map [escape] 'keyboard-quit)
+  ;; (define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
+  ;; (define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
+  ;; (define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
+  ;; (define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
+  ;; (define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
+  ;; (global-set-key [escape] 'evil-exit-emacs-state)
   )
 
 (use-package general
@@ -24,13 +42,18 @@
    "q" 'counsel-ag-thing-at-point
    "r" 'counsel-rg
 
+   "f" '(:wk "file")
    "ff" 'counsel-find-file
    "fF" 'find-file-other-window
    "fr" 'counsel-recentf
 
+   "g" '(:wk "git")
    "gf" 'counsel-git
+
+   "p" '(:wk "project")
    "pf" 'counsel-git
 
+   "b" '(:wk "buff")
    "bb" 'ivy-switch-buffer
    "b/" 'switch-to-buffer-other-window
    "bk" 'kill-buffer
