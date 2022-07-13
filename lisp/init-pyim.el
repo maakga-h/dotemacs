e7e48af4c3d50fcfb4a166300a4114248064a9a2
maakga-h <64649667+maakga-h@users.noreply.github.com>
2022-07-13 23:32:12 +0800

    init pyim

--- lisp/init-pyim.el
+++ lisp/init-pyim.el
@@ -1,6 +1,7 @@
 
 (use-package pyim-basedict)
 (use-package pyim
+  :defer 1 
   :config
   (require 'pyim-cregexp-utils)
   (pyim-basedict-enable)
@@ -17,6 +18,5 @@
   ;; 开启代码搜索中文功能（比如拼音，五笔码等）
   ;; (pyim-isearch-mode 1)
   )
-
 
 (provide 'init-pyim)
