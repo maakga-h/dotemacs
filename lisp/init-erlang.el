5427ddb852d6cdaff29f9062fb1325ec109b6c7e
maakga <64649667+maakga-h@users.noreply.github.com>
2024-07-26 15:15:53 +0800

    auto init erlang mode env

--- lisp/init-erlang.el
+++ lisp/init-erlang.el
@@ -12,7 +12,22 @@
 (defcustom my/erlang-exec-path nil
   "")
 
+(unless my/erlang-root-dir
+  (customize-set-variable 'my/erlang-root-dir (getenv "ERLANG_HOME"))
+  )
+
 (when my/erlang-root-dir
+  (customize-set-variable 'my/erlang-exec-path
+						  (car (directory-files my/erlang-root-dir 'FULL "bin"))
+						  )
+  (customize-set-variable 'my/erlang-tools-path
+						  (car (file-expand-wildcards (expand-file-name "lib/tools-*/emacs" my/erlang-root-dir) 'FULL))
+						  )
+  )
+
+(when (and my/erlang-root-dir
+		   my/erlang-exec-path
+		   my/erlang-tools-path)
   (setq load-path (cons my/erlang-tools-path load-path))
   (setq erlang-root-dir my/erlang-root-dir)
   (setq exec-path (cons my/erlang-exec-path exec-path))
